ba4f71ac038613a19d1a62cb7850477ec593fbf1
Isara <isara.tillekeratne@lseg.com>
2023-04-25 17:20:52 +0530

    Implementation of login view started

---
+++ authapp/src/main/java/com/example/application/views/login/LoginView.java
@@ -0,0 +1,48 @@
+package com.example.application.views.login;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
+import java.awt.*;
+
+@PageTitle("Login")
+@Route(value = "login", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class LoginView extends VerticalLayout {
+
+    public LoginView() {
+        setSpacing(false);
+        setId("login-view");
+        var username = new TextField("Username");
+        var password = new PasswordField("Password");
+        add(
+                new H1("Welcome!"),
+
+                password
+        );
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
